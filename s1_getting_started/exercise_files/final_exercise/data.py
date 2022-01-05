2745199f3ea4a68046f01d5e8fd4208f24c9868c
slangen <steven_steven@hotmail.de>
2022-01-05 12:45:49 +0100

    ex1 done

--- s1_getting_started/exercise_files/final_exercise/data.py
+++ s1_getting_started/exercise_files/final_exercise/data.py
@@ -1,8 +1,38 @@
+from typing_extensions import ParamSpecArgs
 import torch
+from os import walk
+import numpy as np
 
+# PATH="../../../data/corruptmnist/"
 
-def mnist():
+def mnist(PATH,train=True):
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+    filenames = next(walk(PATH), (None, None, []))[2]  # [] if no file
+    train_paths=[PATH+i for i in filenames if 'train' in i]
+    test_paths=[PATH+i for i in filenames if 'test' in i]
+
+    if train:
+        train_images=np.concatenate([np.load(i)['images'] for i in train_paths],axis=0)
+        train_labels=np.concatenate([np.load(i)['labels'] for i in train_paths],axis=0)
+        train = [train_images,train_labels]
+        return train
+    
+    else: 
+        test_images=np.concatenate([np.load(i)['images'] for i in test_paths],axis=0)
+        test_labels=np.concatenate([np.load(i)['labels'] for i in test_paths],axis=0)
+        test = [test_images,test_labels]
+        return test
+
+class Dataset_mnist():
+    def __init__(self,data):
+        self.data=data
+        self.images=torch.from_numpy(self.data[0].copy()).float()
+        self.labels=torch.from_numpy(np.array(self.data[1]).copy())
+
+    def __getitem__(self, idx):
+        
+        return self.images[idx],self.labels[idx]
+
+    def __len__(self):
+        return (len(self.images))
+
--- s1_getting_started/exercise_files/final_exercise/main.py
+++ s1_getting_started/exercise_files/final_exercise/main.py
@@ -2,21 +2,39 @@ import argparse
 import sys
 
 import torch
+from torch import optim, nn
 
-from data import mnist
+from data import mnist, Dataset_mnist
 from model import MyAwesomeModel
+import numpy as np
 
+def _power_of_2(num):
+    """Check if number is power of 2"""
+
+    cond = np.log2(int(num))
+
+    if np.ceil(cond) != np.floor(cond):
+        raise argparse.ArgumentTypeError("Argument must be a power of 2")
+
+    return int(num)
 
 class TrainOREvaluate(object):
     """ Helper class that will help launch class methods as commands
         from a single script
     """
-    def __init__(self):
+    def __init__(self,PATH="../../../data/corruptmnist/"):
+        ##get data
+        self.PATH=PATH
+        
         parser = argparse.ArgumentParser(
             description="Script for either training or evaluating",
             usage="python main.py <command>"
         )
-        parser.add_argument("command", help="Subcommand to run")
+
+        parser.add_argument(
+            "command", choices=["train", "evaluate"], help="Subcommand to run"
+        )
+
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
             print('Unrecognized command')
@@ -29,35 +47,96 @@ class TrainOREvaluate(object):
     def train(self):
         print("Training day and night")
         parser = argparse.ArgumentParser(description='Training arguments')
-        parser.add_argument('--lr', default=0.1)
+        parser.add_argument("--c",'-checkpoint',type=str, default=None,help="checkpoint")
+        parser.add_argument("--lr", type=float, default=0.1, help="learning rate")
+        parser.add_argument("--e", type=int, default=10, help="epoch")
+        parser.add_argument("--b", type=_power_of_2, default=64, help="batch size")
+
         # add any additional argument that you want
         args = parser.parse_args(sys.argv[2:])
         print(args)
-        
+
+        load=args.c
+        epochs=args.e
+        lr=args.lr
+        batchsize=args.b
+
+        train_data= mnist(self.PATH,train=True)
+        trainset=Dataset_mnist(train_data)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchsize, shuffle=True)
+
         # TODO: Implement training loop here
+   
         model = MyAwesomeModel()
-        train_set, _ = mnist()
+
+        if load is None:
+            True
+        else:
+            state_dict = torch.load(load)
+            model.load_state_dict(state_dict)
+
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+
+        train_losses=[]
+
+        for e in range(epochs):
+            running_loss=0
+            model.train()
+            for images, labels in trainloader:
+                optimizer.zero_grad()
+        
+                log_ps = model(images)
+                loss = criterion(log_ps, labels)
+                loss.backward()
+                optimizer.step()
+                
+                running_loss += loss.item()
+            train_losses.append(running_loss)
+            model.eval()
+            print(running_loss)
+            if e%3==0:
+                torch.save(model.state_dict(), 'checkpoint.pth')
+                # torch.save(model.state_dict(), 'checkpoint'+str(e)+'.pth')
+
         
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
-        parser = argparse.ArgumentParser(description='Training arguments')
-        parser.add_argument('load_model_from', default="")
-        # add any additional argument that you want
-        args = parser.parse_args(sys.argv[2:])
-        print(args)
+        # parser = argparse.ArgumentParser(description='Training arguments')
+        # parser.add_argument('load_model_from', default="")
+        # # add any additional argument that you want
+        # args = parser.parse_args(sys.argv[2:])
+        # print(args)
         
+        test_data= mnist(self.PATH,train=False)
+        testset=Dataset_mnist(test_data)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
         # TODO: Implement evaluation logic here
-        model = torch.load(args.load_model_from)
-        _, test_set = mnist()
+        model = MyAwesomeModel()
+        state_dict = torch.load('checkpoint.pth')
+        model.load_state_dict(state_dict)
+
+        # torch.load(args.load_model_from)
+        # _, test_set = mnist()
+
+        with torch.no_grad():
+            # set model to evaluation mode
+            model.eval()
+        accuracies=[]
+
+        for images, labels in testloader:
+            log_ps = model(images)
+            ps = torch.exp(log_ps)
+            pred=torch.max(ps,1)[1]
+            correct= pred==labels.view(*pred.shape)
+            accuracy=correct.type(torch.FloatTensor).mean()
+            accuracies.append(accuracy.item()*100)
+        print(f'Accuracy: {np.mean(accuracies)}%')
+
 
 if __name__ == '__main__':
+    
     TrainOREvaluate()
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    # TOE.train()
+    # TOE.evaluate()
